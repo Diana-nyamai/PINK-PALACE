d57b924bdd036ea8f816a88e6aa6e80b5564fa0d
Diana-nyamai <dnyamai.dn@gmail.com>
2021-09-14 09:00:10 +0300

    update

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ $(function() {
     })
 });
 
+// transparent navbar to colored when scrolled
 $(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
       if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-        $(".navbar-fixed-top").css("background-color", "#c48b9f"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        $(".navbar-fixed-top").css("background-color", "#f8bbd0"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
       } else {
         $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
       }
     });
   });
+
+//   animation of the navbar when clicked
