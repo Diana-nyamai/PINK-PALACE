d292b36bcf99ce8581123802e58e443be1a1bb0f
Diana-nyamai <dnyamai.dn@gmail.com>
2021-09-17 11:52:43 +0300

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ $(function() {
 $(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
       if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-        $(".navbar-fixed-top").css("background-color", "#909497"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        $(".navbar-fixed-top").css("background-color", "#5d5455"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
       } else {
         $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
       }
