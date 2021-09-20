98a1bbd1e9a0760d4884d42f18e6618d48c55731
Diana-nyamai <dnyamai.dn@gmail.com>
2021-09-21 01:55:46 +0300

    slider update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ $(function() {
 $(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
       if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-        $(".navbar-fixed-top").css("background-color", "#5d5455"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        $(".navbar-fixed-top").css("background-color", "#e7bfc0"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
       } else {
         $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
       }
@@ -46,4 +46,19 @@ $(document).ready(function(){
       } 
     }, 4000);
   }
-    
+  
+  // swipper js
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 1,
+      depth: 100,
+      modifier: 2,
+      slideShadows: false,
+    },
+    loop: true
+  });
