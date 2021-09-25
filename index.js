505cac03b094a37975bc9893401460d12493bcf4
Diana-nyamai <dnyamai.dn@gmail.com>
2021-09-25 05:38:44 +0300

    small changes

--- index.js
+++ index.js
@@ -50,3 +50,24 @@ $(document).ready(function(){
     function closeFullImg(){
       fullimgbox.style.display = 'none';
     }
+
+    // animate text
+    var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 3000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
